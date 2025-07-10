fd453b9be95b65b166541d97cd59edb155e2282a
mayli4 <sydsleeper@gmail.com>
2025-07-09 21:27:39 -0400

    feat: crimson ash tiles

--- src/AllBeginningsMod/Common/World/EvilTileCountSystem.cs
+++ src/AllBeginningsMod/Common/World/EvilTileCountSystem.cs
@@ -1,14 +1,21 @@
 using AllBeginningsMod.Content.Tiles.Corruption;
+using AllBeginningsMod.Content.Tiles.Crimson;
 using System;
 using Terraria;
 using Terraria.ModLoader;
+using CorruptAsh = AllBeginningsMod.Content.Tiles.Corruption.CorruptAsh;
 
 namespace AllBeginningsMod.Common.World;
 
 public class EvilTileCountSystem : ModSystem {
     public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts) {
+        //corruption
         Main.SceneMetrics.EvilTileCount += tileCounts[ModContent.TileType<OvergrownCorruptAsh>()] 
                                            + tileCounts[ModContent.TileType<CorruptAsh>()]
                                            + tileCounts[ModContent.TileType<OvergrownCorruptAshFoliage>()];
+        
+        //crimson
+        Main.SceneMetrics.EvilTileCount += tileCounts[ModContent.TileType<CrimsonAshGrass>()]
+                                           + tileCounts[ModContent.TileType<CrimsonAsh>()];
     }
 }
---
+++ src/AllBeginningsMod/Common/World/TileConversions.cs
@@ -0,0 +1,21 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AllBeginningsMod.Common.World;
+
+//todo implement this
+
+public class TileConversions : ModBiomeConversion {
+    public override void PostSetupContent() {
+        // for (int i = 0; i < TileLoader.TileCount; i++) {
+        //     if (TileID.Sets.Conversion.Grass[i] ||
+        //         TileID.Sets.Conversion.GolfGrass[i])
+        //         TileLoader.RegisterConversion(i, Type, HellifyGrass);
+        //
+        //     if (TileID.Sets.Conversion.Dirt[i] ||
+        //         TileID.Sets.Conversion.Stone[i] ||
+        //         TileID.Sets.Conversion.Sand[i])
+        //         TileLoader.RegisterConversion(i, Type, ConvertToAsh);
+        // }
+    }
+}
