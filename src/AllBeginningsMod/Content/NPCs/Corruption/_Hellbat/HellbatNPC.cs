acc533b4167c3ae2b34932c88ff58acace4ac76a
mayli4 <sydsleeper@gmail.com>
2025-07-13 00:12:13 -0400

    feat: ichor lava

--- src/AllBeginningsMod/Content/NPCs/Corruption/_Hellbat/HellbatNPC.cs
+++ src/AllBeginningsMod/Content/NPCs/Corruption/_Hellbat/HellbatNPC.cs
@@ -1,15 +1,30 @@
+using AllBeginningsMod.Common._NPCs;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
 
 namespace AllBeginningsMod.Content.NPCs.Corruption;
 
-public class HellbatNPC : ModNPC {
+public class HellbatNPC : ModNPC, ISpawnsInSwarm{
     public override string Texture => Assets.Assets.Textures.NPCs.Corruption.Hellbat.KEY_HellbatNPC;
-    public enum States {
+    
+    public int SwarmSize() => 0b1010011010;
+    
+    public enum State {
         IdleOnCeiling,
         WakingUp,
         Awake,
+        Dashing,
+        Spitting,
+    }
+
+    public State CurrentState {
+        get => (State)NPC.ai[0];
+        set => NPC.ai[0] = (float)value;
     }
 
     public override void SetStaticDefaults() {
@@ -17,14 +32,18 @@ public class HellbatNPC : ModNPC {
     }
 
     public override void SetDefaults() {
-        NPC.width = 20;
-        NPC.height = 20;
-        NPC.lifeMax = 100;
-        NPC.value = 250f;
+        NPC.width = 40;
+        NPC.height = 30;
+        NPC.lifeMax = 150;
+        NPC.damage = 30;
+        NPC.defense = 10;
+        NPC.value = Item.buyPrice(0, 0, 50, 0);
         NPC.noTileCollide = false;
         NPC.aiStyle = -1;
         NPC.noGravity = true;
         NPC.knockBackResist = 0.05f;
         NPC.friendly = false;
+        NPC.HitSound = SoundID.NPCHit1;
+        NPC.DeathSound = SoundID.NPCDeath2;
     }
 }
---
+++ src/AllBeginningsMod/Core/World/_Lava/_Styles/IchorLavaStyle.cs
@@ -0,0 +1,37 @@
+using AllBeginningsMod.Content.Biomes;
+using AllBeginningsMod.Core.World;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AllBeginningsMod.Core.World;
+
+public class IchorLavafall : ModWaterfallStyle {
+    public override string Texture => Assets.Assets.Textures.Lavas.KEY_IchortLavafall;
+}
+
+public class IchorLava : LavaStyle {
+    public override string LavaTexturePath => Assets.Assets.Textures.Lavas.KEY_IchorLava;
+
+    public override string BlockTexturePath => LavaTexturePath + "_Block";
+
+    public override string SlopeTexturePath => LavaTexturePath + "_Slope";
+
+    public override int ChooseWaterfallStyle() => ModContent.Find<ModWaterfallStyle>("AllBeginningsMod/IchorLavafall").Slot;
+
+    public override int GetSplashDust() => DustID.Ichor;
+
+    public override int GetDropletGore() => 0;
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) {
+        r = 0.0f;
+        g = 0.0f;
+        b = 2.0f;
+    }
+
+    public override void SelectLightColor(ref Color initialLightColor) {
+        initialLightColor = Color.Yellow;
+        initialLightColor.A = 255;
+    }
+}
