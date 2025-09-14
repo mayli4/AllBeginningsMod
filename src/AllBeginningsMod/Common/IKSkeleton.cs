9188b7743b91e3570eae216436e82e8c8e983a77
scss28 <83139747+scss28@users.noreply.github.com>
2025-09-14 19:40:09 +0200

    wip: ng arms

--- src/AllBeginningsMod/Common/IKSkeleton.cs
+++ src/AllBeginningsMod/Common/IKSkeleton.cs
@@ -1,188 +1,91 @@
-﻿using AllBeginningsMod.Utilities;
-using System;
+﻿using System;
 using System.Runtime.CompilerServices;
 
 namespace AllBeginningsMod.Common;
 
-public struct IKConstraints {
-    [Flags]
-    enum Flags {
-        AngleDifference = 1,
-        UpwardOnlyAngle = 2,
-    }
-    record struct AngleDifference(float MaxAngle);
-    record struct UpwardOnlyAngle(float UpAngle);
-
-    Flags _flags;
-    AngleDifference _angleDifference;
-    UpwardOnlyAngle _upwardOnlyAngle;
-
-    public void SetUpwardOnlyAngleConstraint(Vector2? up = null) {
-        _flags |= Flags.UpwardOnlyAngle;
-        _upwardOnlyAngle = new((up ?? -Vector2.UnitY).ToRotation());
-    }
-
-    public void SetAngleDifferenceConstraint(float maxAngle) {
-        _flags |= Flags.AngleDifference;
-        _angleDifference = new(maxAngle);
-    }
-
-    public readonly float Penalties(Span<IKSkeleton.Joint> joints, int index, float completion) {
-        if(index == 0) return 0f;
-
-        float penalties = 0f;
-
-        if((_flags & Flags.AngleDifference) != 0) {
-            float rotation = joints[index].Rotation;
-            float previousRotation = joints[index - 1].Rotation;
-
-            float delta = rotation - previousRotation;
-            delta = (delta + MathF.PI) % (2f * MathF.PI) - MathF.PI;
-
-            penalties += Interpolation.InverseLerp(_angleDifference.MaxAngle, MathHelper.Pi, delta) * 9f;
-        }
-
-        // TODO
-        if((_flags & Flags.UpwardOnlyAngle) != 0) {
-            // float rotation = joints[index].Rotation;
-            // float upAngle = _upwardOnlyAngle.UpAngle;
-
-            // float factor = Interpolation.InverseLerp(0.4f, 0f, completion) * 1500f;
-        }
-
-        return penalties;
-    }
-}
-
 public struct IKSkeleton {
-    public record struct JointOptions(float Length, IKConstraints Constraints = default);
-
-    public IKSkeleton(params ReadOnlySpan<JointOptions> options) {
-        _jointCount = options.Length;
-        for(var i = 0; i < options.Length; i += 1) {
-            _joints[i] = new(options[i].Length, 0f);
-            _constraints[i] = options[i].Constraints;
-        }
+    public struct Constraints() {
+        public float MinAngle = -MathF.PI;
+        public float MaxAngle = MathF.PI;
     }
 
-    public readonly int JointCount => _jointCount;
-    public Vector2 EndPosition { get; private set; }
+    public readonly int JointCount => _options.Length;
+    public readonly int PositionCount => JointCount + 1;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly Vector2 Position(int index) => _positions[index];
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly float Rotation(int index) => _joints[index].Rotation;
+    public IKSkeleton(params (float, Constraints)[] options) {
+        if(options.Length > MaxJointCount) throw new Exception($"MaxJointCount is less than provided options ({options.Length}).");
+        _options = options;
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly float Length(int index) => _joints[index].Length;
+        foreach(var (length, _) in options) _maxDistance += length;
+    }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly Vector2 Offset(int index) => Rotation(index).ToRotationVector2() * Length(index);
+    // http://www.andreasaristidou.com/FABRIK.html 
+    public void Update(Vector2 startPosition, Vector2 targetEndPosition) {
+        _previousPositions = _positions;
 
-    public void Update(Vector2 startPosition, Vector2 targetEndPosition, int stepCount = 400, float updateStep = 0.24f) {
-        Span<float> gradient = stackalloc float[_jointCount];
-        for(var i = 0; i < stepCount; i += 1) {
-            float progress = (float)i / stepCount;
-            for(var j = 0; j < _jointCount; j += 1) {
-                ref Joint joint = ref _joints[j];
-                var initialRotation = joint.Rotation;
-                var initialEndPosition = EndPosition;
-
-                const float HalfDerivativeOffset = 0.00034526f;
-
-                joint.Rotation = initialRotation + HalfDerivativeOffset;
-                RecalculatePositions(startPosition);
-                double lossLeft = LossFunction(
-                    targetEndPosition,
-                    EndPosition,
-                    progress,
-                    j,
-                    _joints[0.._jointCount],
-                    in _constraints[j]
-                );
-
-                joint.Rotation = initialRotation - HalfDerivativeOffset;
-                RecalculatePositions(startPosition);
-                double lossRight = LossFunction(
-                    targetEndPosition,
-                    EndPosition,
-                    progress,
-                    j,
-                    _joints[0.._jointCount],
-                    in _constraints[j]
-                );
-
-                joint.Rotation = initialRotation;
-                EndPosition = initialEndPosition;
-
-                const double InverseDerivativeOffset = 1448.1546;
-                double loss = (lossLeft - lossRight) * InverseDerivativeOffset;
-
-                const double maxAbsoluteValue = 0.17f;
-                gradient[j] = (float)(Math.Tanh(loss / maxAbsoluteValue) * maxAbsoluteValue);
-            }
-
-            float factor = MathF.Sqrt(1f - progress);
-            for(var j = 0; j < _jointCount; j++) {
-                _joints[j].Rotation = MathHelper.WrapAngle(_joints[j].Rotation - gradient[j] * factor * updateStep);
-            }
-
-            RecalculatePositions(startPosition);
-            if(EndPosition.WithinRange(targetEndPosition, 7.4f)) break;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static double LossFunction(
-            Vector2 targetEndPosition,
-            Vector2 endPosition,
-            float completion,
-            int index,
-            Span<Joint> joints,
-            in IKConstraints constraints
-        ) {
-            float distance = targetEndPosition.Distance(endPosition);
-            float penalties = constraints.Penalties(joints, index, completion);
-
-            float total = distance + penalties;
-            if(total <= 0f) return 0f;
-            return MathF.Pow(total, 0.1f);
+        var distance = UpdateInner(startPosition, targetEndPosition);
+        if(distance > 26f) {
+            _positions = _previousPositions;
+            UpdateInner(
+                startPosition,
+                targetEndPosition + startPosition.DirectionTo(targetEndPosition) * startPosition.Distance(_positions[PositionCount - 1])
+            );
         }
     }
 
-    void RecalculatePositions(Vector2 startPosition) {
-        var position = startPosition;
-        for(var i = 0; i < _jointCount; i += 1) {
-            var joint = _joints[i];
+    float UpdateInner(Vector2 startPosition, Vector2 targetEndPosition) {
+        var iterations = 2 << 4;
+        var distance = startPosition.DistanceSQ(targetEndPosition);
+        if(distance > _maxDistance * _maxDistance) iterations = 1;
 
-            _positions[i] = position;
-            position += joint.Rotation.ToRotationVector2() * joint.Length;
+        for(var k = 0; k < iterations; k += 1) {
+            _positions[PositionCount - 1] = targetEndPosition;
+            _positions[0] = startPosition;
+
+            float rootAngle;
+            for(var i = JointCount - 1; i > 0; i -= 1) {
+                var nextAngle = (_positions[i + 1] - _positions[i]).ToRotation();
+
+                rootAngle = (_positions[i] - (i > 1 ? _positions[i - 1] : startPosition)).ToRotation();
+                var angle = rootAngle + Math.Clamp(
+                    MathHelper.WrapAngle(nextAngle - rootAngle),
+                    _options[i].constraints.MinAngle,
+                    _options[i].constraints.MaxAngle
+                );
+
+                _positions[i] = _positions[i + 1] + (angle + MathF.PI).ToRotationVector2() * _options[i].length;
+            }
+
+            rootAngle = 0f;
+            for(var i = 0; i < JointCount; i += 1) {
+                var nextAngle = (_positions[i + 1] - _positions[i]).ToRotation();
+                var angle = rootAngle + Math.Clamp(
+                    MathHelper.WrapAngle(nextAngle - rootAngle),
+                    _options[i].constraints.MinAngle,
+                    _options[i].constraints.MaxAngle
+                );
+
+                _positions[i + 1] = _positions[i] + angle.ToRotationVector2() * _options[i].length;
+                rootAngle = angle;
+            }
+
+            distance = _positions[PositionCount - 1].DistanceSQ(targetEndPosition);
+            if(distance <= 0.01f) break;
         }
 
-        EndPosition = position;
+        return distance;
     }
-
-    public record struct Joint(float Length, float Rotation);
 
     const int MaxJointCount = 16;
 
-    [InlineArray(MaxJointCount)]
-    struct Joints {
-        Joint _;
-    }
+    [InlineArray(MaxJointCount + 1)]
+    struct PositionData { Vector2 _; }
 
-    [InlineArray(MaxJointCount)]
-    struct Positions {
-        Vector2 _;
-    }
-
-    [InlineArray(MaxJointCount)]
-    struct Constraints {
-        IKConstraints _;
-    }
-
-    readonly int _jointCount;
-    Joints _joints;
-    Positions _positions;
-    Constraints _constraints;
+    readonly (float length, Constraints constraints)[] _options;
+    PositionData _previousPositions;
+    PositionData _positions;
+    float _maxDistance;
 }
--- src/AllBeginningsMod/Content/NPCs/Bosses/_Nightgaunt/NightgauntNPC.cs
+++ src/AllBeginningsMod/Content/NPCs/Bosses/_Nightgaunt/NightgauntNPC.cs
@@ -46,14 +46,8 @@ public class NightgauntNPC : ModNPC {
         NPC.noGravity = true;
         NPC.noTileCollide = true;
 
-        IKConstraints rightConstraints = new();
-        rightConstraints.SetAngleDifferenceConstraint(-MathHelper.Pi);
-
-        IKConstraints leftConstraints = new();
-        leftConstraints.SetAngleDifferenceConstraint(MathHelper.Pi);
-
-        _rightArm = new(new(50f), new(50f, rightConstraints));
-        _leftArm = new(new(50f), new(50f, leftConstraints));
+        _rightArm = new((36f, new()), (60f, new() { MinAngle = -MathHelper.Pi, MaxAngle = 0f }));
+        _leftArm = new((36f, new()), (60f, new() { MinAngle = 0f, MaxAngle = MathHelper.Pi }));
     }
 
     public override void AI() {
@@ -63,7 +57,7 @@ public class NightgauntNPC : ModNPC {
         _distanceToTarget = targetDelta.Length();
         _directionToTarget = targetDelta / _distanceToTarget;
 
-        NPC.rotation = Utils.AngleLerp(NPC.rotation, _directionToTarget.ToRotation(), 0.025f);
+        NPC.rotation = Utils.AngleLerp(NPC.rotation, _directionToTarget.ToRotation(), 0.05f);
         NPC.velocity += _directionToTarget * 0.05f;
         NPC.velocity *= 0.95f;
 
@@ -74,11 +68,11 @@ public class NightgauntNPC : ModNPC {
         Vector2 leftShoulderPosition = LeftShoulderPosition;
 
         if(_handSwapTimer == 0) {
-            _handSwapTimer = Main.rand.Next(15, 20);
+            _handSwapTimer = Main.rand.Next(14, 22);
 
             var hSpeed = 2f;
 
-            var grabOffset = 175f;
+            var grabOffset = 160f;
             if(_rightHandSwap) {
                 _rightArmTargetPosition = rightShoulderPosition + _directionToTarget * grabOffset;
                 NPC.velocity += _right * hSpeed;
@@ -90,7 +84,7 @@ public class NightgauntNPC : ModNPC {
 
             _rightHandSwap = !_rightHandSwap;
 
-            NPC.velocity += _directionToTarget * 3.5f;
+            NPC.velocity += _directionToTarget * 2.5f;
         }
         else _handSwapTimer -= 1;
 
@@ -102,8 +96,37 @@ public class NightgauntNPC : ModNPC {
         _leftArm.Update(leftShoulderPosition, _leftArmEndPosition);
     }
 
+    static void DrawArm(Vector2 a, Vector2 b, Vector2 c, Color drawColor, SpriteEffects effects) {
+        var armTexture = Textures.NPCs.Bosses.Nightgaunt.NightgauntArm.Value;
+        Main.spriteBatch.Draw(
+            armTexture,
+            a - Main.screenPosition,
+            new Rectangle(94, 0, armTexture.Width - 94, armTexture.Height),
+            drawColor,
+            (a - b).ToRotation(),
+            new(134 - 94, 12),
+            1f,
+            effects,
+            0f
+        );
+
+        Main.spriteBatch.Draw(
+            armTexture,
+            b - Main.screenPosition,
+            new Rectangle(0, 0, 84, armTexture.Height),
+            drawColor,
+            (b - c).ToRotation(),
+            new(72, 14),
+            1f,
+            effects,
+            0f
+        );
+    }
+
     public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
         if(NPC.IsABestiaryIconDummy) return false;
+        DrawArm(_rightArm.Position(0), _rightArm.Position(1), _rightArm.Position(2), drawColor, SpriteEffects.FlipVertically);
+        DrawArm(_leftArm.Position(0), _leftArm.Position(1), _leftArm.Position(2), drawColor, SpriteEffects.None);
 
 #if DEBUG
         spriteBatch.DrawLine(
@@ -122,37 +145,37 @@ public class NightgauntNPC : ModNPC {
             TextureAssets.BlackTile.Value
         );
 
-        spriteBatch.DrawLine(
-            _rightArm.Position(0) - Main.screenPosition,
-            _rightArm.Position(1) - Main.screenPosition,
-            Color.Blue,
-            4,
-            TextureAssets.BlackTile.Value
-        );
+        // spriteBatch.DrawLine(
+        //     _rightArm.Position(0) - Main.screenPosition,
+        //     _rightArm.Position(1) - Main.screenPosition,
+        //     Color.Blue,
+        //     4,
+        //     TextureAssets.BlackTile.Value
+        // );
 
-        spriteBatch.DrawLine(
-            _rightArm.Position(1) - Main.screenPosition,
-            _rightArm.EndPosition - Main.screenPosition,
-            Color.Red,
-            4,
-            TextureAssets.BlackTile.Value
-        );
+        // spriteBatch.DrawLine(
+        //     _rightArm.Position(1) - Main.screenPosition,
+        //     _rightArm.Position(2) - Main.screenPosition,
+        //     Color.Red,
+        //     4,
+        //     TextureAssets.BlackTile.Value
+        // );
 
-        spriteBatch.DrawLine(
-            _leftArm.Position(0) - Main.screenPosition,
-            _leftArm.Position(1) - Main.screenPosition,
-            Color.Blue,
-            4,
-            TextureAssets.BlackTile.Value
-        );
+        // spriteBatch.DrawLine(
+        //     _leftArm.Position(0) - Main.screenPosition,
+        //     _leftArm.Position(1) - Main.screenPosition,
+        //     Color.Blue,
+        //     4,
+        //     TextureAssets.BlackTile.Value
+        // );
 
-        spriteBatch.DrawLine(
-            _leftArm.Position(1) - Main.screenPosition,
-            _leftArm.EndPosition - Main.screenPosition,
-            Color.Red,
-            4,
-            TextureAssets.BlackTile.Value
-        );
+        // spriteBatch.DrawLine(
+        //     _leftArm.Position(1) - Main.screenPosition,
+        //     _leftArm.Position(2) - Main.screenPosition,
+        //     Color.Red,
+        //     4,
+        //     TextureAssets.BlackTile.Value
+        // );
 #endif
 
         return true;
