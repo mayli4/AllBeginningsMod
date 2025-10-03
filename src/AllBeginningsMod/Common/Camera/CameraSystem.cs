3f1ae604f43f891f093492d684981146c07d60b6
mayli4 <sydsleeper@gmail.com>
2025-10-03 15:35:12 -0400

    wip: camera stubs

---
+++ src/AllBeginningsMod/Common/Camera/CameraSystem.cs
@@ -0,0 +1,11 @@
+﻿using AllBeginningsMod.Common.Config;
+
+namespace AllBeginningsMod.Common.Camera;
+
+internal struct CameraModifierDescription() {
+    public float? Zoom { get; set; } = null;
+}
+
+internal sealed class CameraSystem {
+    bool _shakeEnabled = ClientConfig.Instance.EnableScreenshake;
+}
---
+++ src/AllBeginningsMod/Common/Config/ClientConfig.cs
@@ -0,0 +1,8 @@
+﻿using Terraria.ModLoader.Config;
+
+namespace AllBeginningsMod.Common.Config;
+
+internal partial class ClientConfig : ModConfig {
+    public override ConfigScope Mode => ConfigScope.ClientSide;
+    public static ClientConfig Instance => ModContent.GetInstance<ClientConfig>();
+}
