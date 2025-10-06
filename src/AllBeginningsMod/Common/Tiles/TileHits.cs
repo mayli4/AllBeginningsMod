5f3fe6f7c51211a636ffadb609da0b645f9c2006
mayli4 <sydsleeper@gmail.com>
2025-10-05 20:28:41 -0400

    wip: tile hits

---
+++ src/AllBeginningsMod/Common/Tiles/TileHits.cs
@@ -0,0 +1,68 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Collections.Generic;
+
+namespace AllBeginningsMod.Common.Tiles;
+
+// https://github.com/tModLoader/tModLoader/issues/4735
+
+internal sealed class TileHits : ModSystem {
+    internal readonly record struct TileHitCtx(HitTile Tile, int X, int Y, int HitType, Player player);
+    
+    public Dictionary<Point, TileHitCtx> Contexts { get; } = new();
+
+    public override void OnModLoad() {
+        base.OnModLoad();
+
+        // We should ensure this is updated for 1.4.4.
+        IL_Player.PlaceThing_TryReplacingTiles += RewriteHitObjectInvocations;
+        IL_Player.ItemCheck_UseMiningTools_ActuallyUseMiningTool += RewriteHitObjectInvocations;
+        IL_Player.ItemCheck_UseMiningTools_TryHittingWall += RewriteHitObjectInvocations;
+        IL_Player.GetOtherPlayersPickTile += RewriteHitObjectInvocations;
+        IL_Player.PickTile += RewriteHitObjectInvocations;
+        IL_Player.HasEnoughPickPowerToHurtTile += RewriteHitObjectInvocations;
+    }
+
+    private void RewriteHitObjectInvocations(ILContext il) {
+        var c = new ILCursor(il);
+
+        c.Index = c.Instrs.Count - 1;
+
+        // Jump to every HitTile::HitObject invocation, push Player instance (`this`, ldarg.0), and call our method instead of HitTile::HitObject (jump to label after).
+        while (c.TryGotoPrev(MoveType.After, x => x.MatchCallvirt<HitTile>(nameof(HitTile.HitObject)))) {
+            // Mark label to jump to later.
+            var label = c.MarkLabel();
+
+            // Go to before call
+            c.Index--;
+
+            // Emit `this` (Player instance), then invoke our own method
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate(HitObjectWithPlayerContext);
+
+            // Jump to label after the call
+            c.Emit(OpCodes.Br, label);
+        }
+    }
+
+    public override void Unload() {
+        base.Unload();
+
+        Contexts.Clear();
+    }
+
+    public override void PostUpdateEverything() {
+        base.PostUpdateEverything();
+
+        Contexts.Clear();
+    }
+
+    public bool TryGetHitTileContext(Point point, out TileHitCtx context) {
+        return Contexts.TryGetValue(point, out context);
+    }
+
+    private static int HitObjectWithPlayerContext(HitTile hitTile, int x, int y, int hitType, Player player) {
+        ModContent.GetInstance<TileHits>().Contexts[new Point(x, y)] = new TileHitCtx(hitTile, x, y, hitType, player);
+        return hitTile.HitObject(x, y, hitType);
+    }
+}
---
+++ src/AllBeginningsMod/Content/Jungle/_Tiles/TestBigTree.cs
@@ -0,0 +1,83 @@
+﻿using AllBeginningsMod.Common.World;
+using Terraria.ID;
+using Terraria.Localization;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+
+namespace AllBeginningsMod.Content.Jungle;
+
+public class TestBigTree : ModTile {
+    public override string Texture => Textures.Tiles.Jungle.KEY_TestBigTree;
+
+    public override void SetStaticDefaults() {
+        Main.tileMergeDirt[Type] = false;
+        Main.tileBlockLight[Type] = false;
+        Main.tileSolid[Type] = false;
+        
+        Main.tileFrameImportant[Type] = true;
+        Main.tileAxe[Type] = true;
+        MineResist = 1f;
+        
+        TileID.Sets.IsATreeTrunk[Type] = true;
+        TileID.Sets.PreventsTileRemovalIfOnTopOfIt[Type] = true;
+        TileID.Sets.PreventsTileReplaceIfOnTopOfIt[Type] = true;
+
+        DustType = DustID.RichMahogany;
+    }
+
+    public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak) {
+        return base.TileFrame(i, j, ref resetFrame, ref noBreak);   
+    }
+    
+    public static bool GrowTree(int x, int y, bool needsSapling = true, int generateOverrideHeight = 0) {
+        var type = ModContent.TileType<TestBigTree>();
+
+        void GenBaseOnlyOutermost(int i, int j) {
+            WorldGen.PlaceTile(i - 1, j, type, mute: true, forced: true);
+            WorldGen.PlaceTile(i + 3, j, type, mute: true, forced: true);
+        }
+        
+        int minHeightSegments = 20;
+        int maxHeightSegments = 40; 
+        
+        int treeHeightSegments = generateOverrideHeight > 0 ? generateOverrideHeight : WorldGen.genRand.Next(minHeightSegments, maxHeightSegments + 1);
+
+        GenBaseOnlyOutermost(x, y);
+
+        for (int h = 0; h < treeHeightSegments; h++) {
+            int currentY = y - h;
+
+            WorldGen.PlaceTile(x, currentY, type, mute: true, forced: true);
+            WorldGen.PlaceTile(x + 1, currentY, type, mute: true, forced: true);
+            WorldGen.PlaceTile(x + 2, currentY, type, mute: true, forced: true);
+        }
+
+        int frameStartX = x - 1; 
+        int frameWidth = 5;      
+        for (int h = 0; h < treeHeightSegments; h++) {
+            for (int iOffset = 0; iOffset < frameWidth; iOffset++) {
+                 WorldGen.SquareTileFrame(frameStartX + iOffset, y - h, true);
+            }
+            
+            if (h == 0) { 
+                frameStartX = x;
+                frameWidth = 3;
+            }
+        }
+        
+        return true;
+    }
+}
+
+internal class FrameTest : ModSystem {
+    public override void PostUpdateEverything() {
+        Vector2 mouseWorldPosition = Main.MouseWorld;
+        int tileX = (int)(mouseWorldPosition.X / 16f);
+        int tileY = (int)(mouseWorldPosition.Y / 16f);
+
+        if(Main.keyState.IsKeyDown(Keys.H) && !Main.oldKeyState.IsKeyDown(Keys.H)) {
+            //WorldGen.TileFrame(tileX, tileY, true);
+            
+            TestBigTree.GrowTree(tileX, tileY);
+        }
+    }
+}
